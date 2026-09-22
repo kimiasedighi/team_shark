e1615c66c830dcef00a318ea373fdac0b651825e
seraphina <st183450@stud.uni-stuttgart.de>
2026-09-22 15:32:47 +0200

    add memory manager

--- main_shark.py
+++ main_shark.py
@@ -1,11 +1,24 @@
 import numpy as np
 import fa2026 as fa
 
+from memory import set_memory, get_memory
+
 rng = np.random.default_rng(1)
 
+
+
 def shark_rule(observation, memory):
+
+    found_target = get_memory(memory)
+    
+
+
+
+
+
     return fa.Action(
         push=rng.uniform(0.0, 0.2),
         move=tuple(rng.uniform(-0.3, 0.3, size=2)),
         pheromones=(rng.uniform(0.0, 0.01),),
     ), memory
+
